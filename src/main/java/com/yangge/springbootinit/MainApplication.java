2edc0efda9e1fb63544c945ef3196c5c1d96fe6d
youthOfMe <2040690180@qq.com>
2024-07-05 23:44:31 +0800

    feat: 完成请求日志拦截器

--- src/main/java/com/yangge/springbootinit/MainApplication.java
+++ src/main/java/com/yangge/springbootinit/MainApplication.java
@@ -3,9 +3,11 @@ package com.yangge.springbootinit;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @MapperScan("com.yangge.springbootinit.mapper")
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class MainApplication {
 
     public static void main(String[] args) {
